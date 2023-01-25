cc64563434bff90a50271bc03d5fd114613d0333
hchalke <hchalke@my.harrisburgu.edu>
2023-01-24 20:51:06 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ function App() {
         <div className="container">
           <h1>Hello World!</h1>
           <p>This is Hardik Chalke</p>
+          <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+          Learn React
+        </a>
         </div>
       </header>
     </div>
@@ -16,3 +24,5 @@ function App() {
 }
 
 export default App;
+
+

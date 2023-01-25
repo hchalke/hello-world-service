c13ab9ab805330e183b4cdf7b3f0a1bc1ca14f59
hchalke <hchalke@my.harrisburgu.edu>
2023-01-24 20:39:41 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div classname="container">
+        <div className="container">
           <h1>Hello World!</h1>
           <p>This is Hardik Chalke</p>
         </div>
